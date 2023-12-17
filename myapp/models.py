930959db962d994f3b1519c159a0b79ae57739a1
npaudyal321 <naween321@gmail.com>
2023-12-17 14:11:12 +0545

    model introduction

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):  # class name later becomes the table name in the database. appname_classname
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    address = models.CharField(max_length=50)
+    email = models.EmailField(max_length=20)
+
+
+# Student.objects.create(name="Jon", age=30, email="kdns@nds.com", address="KTM")
+# # INSERT INTO Student (NAME, EMAIL) VALUES ("fnd", )
+#
+#
+# Student.objects.all()
+# # Select * from student
