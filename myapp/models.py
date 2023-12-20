50e3daaf4f482ed4626a22eb6bb678bef4f90c9d
npaudyal321 <naween321@gmail.com>
2023-12-20 15:02:51 +0545

    model relations

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 
+class ClassRoom(models.Model):
+    name = models.CharField(max_length=10)
+
+
 class Student(models.Model):  # class name later becomes the table name in the database. appname_classname
     name = models.CharField(max_length=20)
     age = models.IntegerField()
     address = models.CharField(max_length=50)
     email = models.EmailField(max_length=20)
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="classroom_students")
 
 
 # Student.objects.create(name="Jon", age=30, email="kdns@nds.com", address="KTM")
