23b4fe6185b004a589d1010bae310df98eba9a5a
npaudyal321 <naween321@gmail.com>
2023-12-27 15:03:33 +0545

    add student

--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from myapp.models import ClassRoom
+from myapp.models import ClassRoom, Student
 
 
 def add_classroom(request):
@@ -10,3 +10,15 @@ def add_classroom(request):
     classrooms = ClassRoom.objects.all()
     return render(request, template_name="forms/classroom.html", context={"classrooms": classrooms})
 
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        email = request.POST.get("email")
+        address = request.POST.get("address")
+        classroom_id = request.POST.get("classroom_id")
+        Student.objects.create(name=name, age=age, email=email, address=address, classroom_id=classroom_id)
+        return redirect("student")
+    classrooms = ClassRoom.objects.all()
+    return render(request, template_name="forms/add_student.html", context={"classrooms": classrooms})
