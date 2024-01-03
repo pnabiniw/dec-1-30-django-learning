1853370582a731a606a07a407abc09d24208fb09
npaudyal321 <naween321@gmail.com>
2024-01-03 14:47:30 +0545

    cb crud

--- classbased/urls.py
+++ classbased/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, ClassRoomView, StudentView
+from .views import HomeView, ClassRoomView, StudentView, delete_classroom
 
 
 urlpatterns = [
     path("home/", HomeView.as_view(), name="classbased_home"),
     path("classroom/", ClassRoomView.as_view(), name="classbased_classroom"),
     path("student/", StudentView.as_view(), name="classbased_student"),
+    path("delete-classroom/<int:id>/", delete_classroom, name="classbased_delete_classroom")
 ]
--- classbased/views.py
+++ classbased/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, CreateView
 from myapp.models import ClassRoom, Student
+from django.urls import reverse_lazy
 from .forms import ClassRoomModelForm
 
 
@@ -12,10 +13,25 @@ class ClassRoomView(CreateView):
     form_class = ClassRoomModelForm
     template_name = "classbased/classroom.html"
     queryset = ClassRoom.objects.all()
-    context_object_name = "classrooms"
+    success_url = reverse_lazy('classbased_classroom')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()  # {"form": , "view": }
+        print(context)
+        context["classrooms"] = ClassRoom.objects.all()
+        return context
 
 
 class StudentView(ListView):
     template_name = "classbased/student.html"
     queryset = Student.objects.all()
     context_object_name = "students"
+
+
+def delete_classroom(request, id):
+    classroom = ClassRoom.objects.get(id=id)
+    if request.method == "POST":
+        classroom.delete()
+        return redirect("classbased_classroom")
+    return render(request, template_name="classbased/delete_classroom.html",
+                  context={"classroom": classroom})
